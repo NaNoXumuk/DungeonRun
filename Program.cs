53026cf3665fa2f9b91bc014d4b22602d2dd041f
NaNoXumuk <famassamaf@gmail.com>
2024-08-02 16:08:07 +0900

    Жоско пока не получается :(

--- Program.cs
+++ Program.cs
@@ -10,58 +10,53 @@ namespace DungeonRun
     {
         static void Main(string[] args)
         {   int x=1,y=1;
-            char[,] Map =
-            {           { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#' },
-                        { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' }
-            };
+            char[,] Map = new char[30, 200];
+            
             Console.CursorVisible = false;
             Console.SetCursorPosition(0, 0);
+            Console.WindowHeight = Console.LargestWindowHeight;
+            Console.WindowWidth = Console.LargestWindowWidth;
+            
+            //for (int i = 0; i < MapTest.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < MapTest.GetLength(1) ; j++)                
+            //        Console.Write(MapTest[i, j]);                
+            //Console.WriteLine();
+            //}
             for (int i = 0; i < Map.GetLength(0); i++)
             {
-                for (int j = 0; j < Map.GetLength(1) - 1; j++)
-                {
-                    Console.Write(Map[i, j]);
-                }
-            Console.WriteLine();
+                for (int j = 0; j < Map.GetLength(1); j++)
+                    Map[i,j]='-';
+                Console.WriteLine();
             }
-            Console.SetCursorPosition(x, y);
+
+
+
+
+
+
+                Console.SetCursorPosition(x, y);
             Console.Write('@');
+            Console.ReadKey();
             while (true)
             {
-            switch(Console.ReadKey().Key)
+
+                ConsoleKeyInfo WayToGo = Console.ReadKey();
+                switch (WayToGo.Key)
                 {
-                    case ConsoleKey.W: Map[y,x] = ' '; Console.SetCursorPosition(x, y--); Map[y,x] = '@'; break;
-                    case ConsoleKey.A: Map[y,x] = ' '; Console.SetCursorPosition(x--, y); Map[y,x] = '@'; break;
-                    case ConsoleKey.S: Map[y,x] = ' '; Console.SetCursorPosition(x, y++); Map[y,x] = '@'; break;
-                    case ConsoleKey.D: Map[y,x] = ' '; Console.SetCursorPosition(x++, y); Map[y,x] = '@'; break;
+                    case ConsoleKey.W: if (Map[y - 1, x] != '#') { Map[y, x] = ' '; Console.Write(' '); Console.SetCursorPosition(x, y--); Map[y, x] = '@'; Console.Write('@'); } break;
+                    case ConsoleKey.A: if (Map[y, x - 1] != '#') { Map[y, x] = ' '; Console.Write(' '); Console.SetCursorPosition(x--, y); Map[y, x] = '@'; Console.Write('@'); } break;
+                    case ConsoleKey.S: if (Map[y + 1, x] != '#') { Map[y, x] = ' '; Console.Write(' '); Console.SetCursorPosition(x, y++); Map[y, x] = '@'; Console.Write('@'); } break;
+                    case ConsoleKey.D: if (Map[y, x + 1] != '#') { Map[y, x] = ' '; Console.Write(' '); Console.SetCursorPosition(x++, y); Map[y, x] = '@'; Console.Write('@'); } break;
                 }
-            Console.Clear();
-                for (int i = 0; i < Map.GetLength(0); i++)
-                {
-                    for (int j = 0; j < Map.GetLength(1) - 1; j++)
-                    {
-                        Console.Write(Map[i, j]);
-                    }
-                    Console.WriteLine();
-                }
+                for (int i = y; i < y + 3; i++)
+                    for (int j = x; j < x + 3; j++)
+                        if (Map[i, j] == 'w' || Map[i, j] == 'a' || Map[i, j] == 's' || Map[i, j] == 'd')
+                            Console.Write(Map[i, j]);
+                        
+
+
             }
-           
-            
-             
         }
     }
 }
